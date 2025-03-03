1318d7d4e614a00cb5ea5bc3f7ccd1fb57245277
Yashwanth M S <134080803+Yashwanth-79@users.noreply.github.com>
2025-03-04 01:10:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 __import__('pysqlite3')
 import sys
+import streamlit as st
+import os
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 os.environ["STREAMLIT_SERVER_MODULE_WATCHER_DISABLE_MODULES"] = "torch,transformers,langchain"
 
-import streamlit as st
-import os
+
 import json
 import markdown
 import base64
